124ebb0379339798d0f7c923d28b7b627294e7f6
DESKTOP-LL5MEFR\jorge <jorgepinilla98@gmail.com>
2021-10-26 23:28:10 +0200

    Los objetivos son: Familiarizarse con Python como lenguaje de programación Gestionar datos procedentes de comunicaciones serie Trabajar con estos datos en formato csv y json para almacenarlos en ficheros y redireccionarlos vía internet Operar con los datos y representarlos gráficamente

--- Practica3.py
+++ Practica3.py
@@ -5,28 +5,27 @@ Spyder Editor
 This is a temporary script file.
 """
 
+ #importo las librerias
+ 
 import serial
 import time
 import csv
 import matplotlib
-# matplotlib.use("tkAgg")
+import re
 import matplotlib.pyplot as plt
 import numpy as np
 
-arduino = serial.Serial('COM4', baudrate = 115200, timeout = 1.0)
+#defino el objeto serial
+arduino = serial.Serial('COM3', baudrate = 115200, timeout = 1.0)
 
 time1 = 0;
 
+#inicializo variables
 x = np.array([0])
 y = np.array([0])
 z = np.array([0])
 
 data = ""
-
-inputLine = '0.07,0.5,1.5'
-
-fig, ax = plt.subplots()
-
 
 
 
@@ -34,17 +33,22 @@ while True:
     
      try:
          
-         
+         #si tiene para leer en el buffer
         if (arduino.inWaiting() > 0):
     
         
             try:
-                    
-                line = str(inputLine) #str(arduino.readline())
+                    #lee una linea del buffer
+                line = arduino.readline()
+                #pasamos los datos a float
+                line = line.decode('utf-8') #convert byte to str
+                data = [float(s) for s in re.findall(r'-?\d+\.?\d*', line)]
                 
             except:
                     
                 continue
+            
+            #guardar datos en .cvs
         
             # with open("test_data.csv","a") as f:
                 
@@ -52,14 +56,14 @@ while True:
             #     writer.writerow([line])
                     
                 
-            data =  line.split(',')
             
-            x = np.append([x],[float(data[0])])
-            y = np.append([y],[float(data[1])])
-            z = np.append([z],[float(data[2])])
+            #almacenamos datos en arryas
+            x = np.append([x],[round(data[0],2)])
+            y = np.append([y],[round(data[1],2)])
+            z = np.append([z],[round(data[2],2)])
             
            
-                        
+                        #cuando han pasado 5 sec calculamos media y varianza y plotemos datos de entrada 
                     
             if(time.time() - time1 > 5):
                 
@@ -75,15 +79,26 @@ while True:
                 
                 print("X STD :", xDes)
                 
+                plt.subplot(1,3,1)
                 
-                ax.plot(tiempo, media, color = 'tab:purple')
-                ax.plot(tiempo, varianza, color = 'tab:green')
+                plt.plot(x, color = 'tab:red')
+                
+                plt.subplot(1,3,2)
+                
+                plt.plot(y, color = 'tab:green')
+                
+                plt.subplot(1,3,3)
+                
+                plt.plot(z, color = 'tab:blue')
+                
                 plt.show()
                     
                     
                 x = np.array([0])
                 y = np.array([0])
                 z = np.array([0])
+                
+                
                     
                 time1 = time.time()
                 
@@ -93,8 +108,8 @@ while True:
            
      except:
          arduino.close()
-         print("Keyboard Interrupt")
          break
+         
         
         
 
